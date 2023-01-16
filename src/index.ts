bf6708317852bb0953d242948319af0e770d6d05
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2023-01-16 22:40:27 +0600

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import useReflare from 'reflare';
+
+const handleRequest = async (
+  request: Request,
+): Promise<Response> => {
+  const reflare = await useReflare();
+
+  reflare.push({
+    path: '/*',
+    upstream: {
+      domain: 'httpbin.org',
+      protocol: 'https',
+    },
+  });
+
+  return reflare.handle(request);
+};
+
+addEventListener('fetch', (event) => {
+  event.respondWith(handleRequest(event.request));
+});
